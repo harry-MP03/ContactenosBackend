4857b534af5386d9b4ce5b009ef798bc6e3abd12
Cristopher <harrintongpoveda03@gmail.com>
2025-09-19 16:15:48 -0600

    SCM_DO_BUILD_DURING_DEPLOYMENT

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,7 @@
 from .settings import *
 import os
 
-# --- CONFIGURACIÓN DE PRODUCCIÓN ---
+# --- CONFIGURACIÓN DE PRODUCCIÓN ---__
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
