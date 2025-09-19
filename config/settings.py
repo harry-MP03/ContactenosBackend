92621a1550ae700c62092200c60794ff0e9c237a
Cristopher <harrintongpoveda03@gmail.com>
2025-09-19 12:13:54 -0600

    Configurando STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,8 @@ from Apps.Seguridad.settings_apps import Seguridad_Settings_Apps
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
